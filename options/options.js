043f0332a3f6ab55b9811198a2a914c91ea7d83c
Crowdbotics <team@crowdbotics.com>
2021-09-30 21:56:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "eerie_pulp_31010",
+  url: "https://eerie_pulp_31010.botics.co",
+  api: "https://eerie_pulp_31010.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
